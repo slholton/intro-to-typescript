3dd2de6876ff47045af7f279b87b60eb07009a31
Shavonne Holton <shavonne.holton@gmail.com>
2021-09-05 21:39:57 -0700

    added classes lesson

--- build/classes.js
+++ build/classes.js
@@ -1,2 +1,38 @@
 "use strict";
 // interface
+class Mail {
+    constructor(data // "public" can be accessed anywhere without any restrictions
+    ) {
+        this.data = data;
+        this._password = undefined;
+    }
+    set password(x) {
+        this._password = x;
+    }
+    get password() {
+        return this._password;
+    }
+    send() {
+        let returnValue = "success";
+        return returnValue;
+    }
+}
+class Email {
+    constructor(data) {
+        this.data = data;
+    }
+    send() {
+        let returnValue = "success";
+        return returnValue;
+    }
+    myMessage() {
+        return JSON.stringify(this);
+    }
+}
+const messages = [
+    new Mail("Hello from snail mail"),
+    new Email("Hello can you see me in the inbox")
+];
+messages.forEach(m => m.send());
+let email = new Email("Hey!");
+console.log(email.myMessage());
--- src/classes.ts
+++ src/classes.ts
@@ -1,1 +1,68 @@
 // interface
+
+interface Point {
+    calc(): number
+}
+
+interface Point {
+    something(): number
+}
+
+// With interfaces you can add them to classes with the keyword
+// implements
+
+type message = "success" | "failure"
+interface Send {
+    data: string
+    send(): message
+}
+
+// getMessage
+interface GetMessage {
+    myMessage(): string
+}
+
+class Mail implements Send{ // implements enforce particular contracts
+    private _password: string | undefined
+    
+    constructor(
+        public data: string // "public" can be accessed anywhere without any restrictions
+        ){
+            this._password = undefined
+        } 
+
+    set password(x: string | undefined){
+        this._password = x
+    }
+
+    get password(): string | undefined {
+        return this._password
+    }
+
+    send(): message {
+        let returnValue: message = "success"
+        return returnValue
+    }
+}
+
+class Email implements Send, GetMessage {
+    data
+    constructor(data: string){
+        this.data = data
+    }
+    send(): message {
+        let returnValue: message = "success"
+        return returnValue
+    }
+    myMessage(): string {
+        return JSON.stringify(this)
+    }
+}
+
+const messages = [
+    new Mail("Hello from snail mail"), 
+    new Email("Hello can you see me in the inbox")]
+
+messages.forEach(m => m.send())
+let email = new Email("Hey!")
+console.log(email.myMessage())
